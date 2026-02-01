1ccd0e3248765e28795ba99658475686441d9ef8
tanmoypal11 <109018359+tanmoypal11@users.noreply.github.com>
2026-02-01 13:25:29 +0530

    Update llm_helper.py

--- llm_helper.py
+++ llm_helper.py
@@ -4,6 +4,6 @@ from transformers import pipeline
 @st.cache_resource(show_spinner=False)
 def load_llm():
     return pipeline(
-        task="text2text-generation",
+        task="text-generation",
         model="google/flan-t5-small"
     )
